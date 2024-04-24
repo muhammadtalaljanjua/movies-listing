bf9406fd1ebd4ccc4a4bd4cc07586d35d2052a17
Muhammad Talal <hafiztalalaqeel1@gmail.com>
2024-04-24 19:18:45 +0500

    Database Connected

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://root:root@movies-listing.wwbwufi.mongodb.net/movies-collection?retryWrites=true&w=majority&appName=Movies-Listing";
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,22 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log("Application is using PORT: ", PORT);
-});
-
 app.get("/", (request, response) => {
   console.log(request);
-  return response.status(200).send("Connection Established");
+  return response.status(200).send("Connection Established.");
 });
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("Database is connected.");
+    app.listen(PORT, () => {
+      console.log("Application is using PORT:", PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
