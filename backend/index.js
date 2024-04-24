1f9f0109b877f9fe3d0bf5e2d29df007002d6107
Muhammad Talal <hafiztalalaqeel1@gmail.com>
2024-04-24 18:54:25 +0500

    Port Connected

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,13 @@
 import express from "express";
 import { PORT } from "./config.js";
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log("Application is using PORT: ", PORT);
+});
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(200).send("Connection Established");
+});
